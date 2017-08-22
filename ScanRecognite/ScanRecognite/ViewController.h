d388b51fea106e42c8f385a8ad8f8fb1c8eda07a
hbj <hbj@hbjtekiMac-mini.local>
2017-08-22 11:02:47 +0800

    tag1.0

---
+++ ScanRecognite/ScanRecognite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanRecognite
+//
+//  Created by hbj on 2017/8/22.
+//  Copyright © 2017年 宝剑. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
